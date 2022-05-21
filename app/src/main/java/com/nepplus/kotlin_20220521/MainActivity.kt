647bc5704118138a5ce68b17d181ed2e6b39dd30
이무한 <klaw96@naver.com>
2022-05-21 10:48:46 +0900

    x로그눌러보기

--- app/src/main/java/com/nepplus/kotlin_20220521/MainActivity.kt
+++ app/src/main/java/com/nepplus/kotlin_20220521/MainActivity.kt
@@ -2,11 +2,21 @@ package com.nepplus.kotlin_20220521
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLog.setOnClickListener {
+
+            Log.d("메인화면", "화면눌림")
+
+
+
+        }
 
 
 
