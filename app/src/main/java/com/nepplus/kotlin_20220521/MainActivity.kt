9118aa901efd7faf90f0a2861d8afba9fa174028
이무한 <klaw96@naver.com>
2022-05-21 12:25:45 +0900

    입력 /버튼 /출력 구조 UI 작성

--- app/src/main/java/com/nepplus/kotlin_20220521/MainActivity.kt
+++ app/src/main/java/com/nepplus/kotlin_20220521/MainActivity.kt
@@ -95,6 +95,45 @@ class MainActivity : AppCompatActivity() {
 
             }
 
+//              조건식 논리 연산 예시.
+
+//             1. 급여높음 2. 거리가 가까움 3. 근로시간 적으면.
+
+            val salary = 7800
+            val minutes = 80
+            val workhour = 35
+
+
+
+
+//              연봉 4000이상 and 거리는 50분 이내.
+
+            if ( salary >=4000 && minutes<50) {
+
+                Log.d("조건문","연봉&거리 모두 만족")
+
+
+            }
+//            거리가 20분 이내 or 근무시간 40시간 이내
+
+            if(minutes<=20 || workhour <=40){
+
+
+
+            }
+
+//            근무시간이 50시간 이상만 아니면 된다.
+            if(!workhour>=50) {
+
+
+
+
+
+            }
+
+
+
+
 
 
 
